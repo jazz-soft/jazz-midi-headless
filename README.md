67a2f111726bb1530b08341e910349f0a9f4889e
Sema <jazz-dev@jazz-soft.net>
2021-04-16 00:18:16 -0400

    Added test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jazz-midi-headless
 coming soon...
+
+[![npm](https://img.shields.io/npm/v/jazz-midi-headless.svg)](https://www.npmjs.com/package/jazz-midi-headless)
+[![build](https://github.com/jazz-soft/jazz-midi-headless/actions/workflows/build.yml/badge.svg)](https://github.com/jazz-soft/jazz-midi-headless/actions)
--- test/player.js
+++ test/player.js
@@ -1,14 +1,15 @@
 const JZZ = require('jzz');
 const jmh = require('..')(JZZ);
 const puppeteer = require('puppeteer');
-const url = 'file://' + __dirname + '/test.html';
+const url = 'file://' + __dirname + '/player.html';
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on('console', msg => console.log('>>', msg.text()));
+  page.on('console', msg => {
+    console.log('>>', msg.text());
+    if (msg.text() == 'Done!') browser.close();
+  });
   await jmh.setup(page);
   await page.goto(url);
-  await page.waitForTimeout(5000);
-  await browser.close();
 })();
